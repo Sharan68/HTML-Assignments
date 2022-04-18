580b6446497a0218220c4679a59d480b5ff393a9
Sharan68 <76605498+Sharan68@users.noreply.github.com>
2022-04-18 19:01:12 +0530

    Add files via upload

---
+++ Assignment-6/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#submit").click(function(){
+        $("#userName").innerText = "Sharan";
+        $("#userEmail").innerText = "sharan.bejugam@in.ey.com";
+        $("#usernumber").innerText = "9912743500";
+        $("#note").css("color", "coral");
+    });  
+  });
